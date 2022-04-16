0a29bcda781bef6f14db91f8fbc22f163e77a6aa
sabrinaValansi <sabrivalan@hotmail.com>
2022-04-16 16:12:20 -0300

    Examen completo

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-console.log("Hola");
-console.log("Chau");
+import {factura} from "./src/facturas.js";
 
-//import {factura} from "./src/facturas.js";
-//import fs from './textos/texto.txt';
+
 
 //console.log(factura);
 
@@ -13,7 +11,7 @@ factura.sort((a,b)=>a.importe-b.importe);
 console.log(factura);
 console.log("----ORDEN MAYOR A MENOR----");
 factura.sort((b,a)=>a.importe-b.importe);
-console.log(factura);
+console.log(factura);clearScreenDown
 
 console.log("----filtro importe mayor a 2000----");
 const facturasFiltradas= factura.filter(dato => dato.importe>2000);
@@ -23,5 +21,30 @@ console.log("----nuevo objeto----");
 const nuevaFactura = factura.map(dato => {return{num:dato.numero,imp:dato.importe*2}});
 console.log(nuevaFactura);
 
-/* const data = fs.readFileSync( './textos/text.txt' , 'utf-8' )
-console .log(data) */
+console.log("----Acumulador----");
+let sumaFacturas = factura.reduce((acumulador,fact)=>acumulador+fact.importe,0);
+console.log(sumaFacturas);
+
+console.log("------ARCHIVO DE TEXTO------");
+import fs from 'fs'
+import { clearScreenDown } from "readline";
+
+const textoNuevo = '\nesto es un texto nuevo';
+
+fs.appendFileSync( './textos/texto.txt' , textoNuevo ) ;
+
+const datos = fs.readFileSync('./textos/texto.txt','utf-8');
+
+console.log(datos);
+
+const diAlgo = (texto) => console.log(texto);
+
+setTimeout(()=> {
+    diAlgo("Pasaron 3 segundos");
+    console.log("Terminado");
+},3000);
+setTimeout(()=> {
+    diAlgo("Pasaron 7 segundos");
+    console.log("Terminado");
+},7000);
+
